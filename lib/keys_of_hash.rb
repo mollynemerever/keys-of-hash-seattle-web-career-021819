c4d57d8b9d2a4f78ef609a35ed1b04d7a0cedcc5
github username <github email address>
2019-02-06 12:57:33 -0800

    keys of hash solution

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)  #splat added so number of arguments not specified
+
+   output_array = []
+     arguments.each do |country|  #in case input is several arguments(countries, loop through input like an array)
+      self.each do |animal_key, country_value|  #.self tells method to go through the hash (in this case animals)
+
+       if country_value == country   #if the country_value inside hash equals the country input from arguments
+       output_array << animal_key  #add the corresponding animal_key to output hash 
+       end
+      end
+    end
+    return output_array
+ end
 end
